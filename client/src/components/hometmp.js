a0bf255652b1da58e1aff978b7fa34112482d7ca
Huzaifa-31 <31huzafasameer@gmail.com>
2022-09-14 17:58:06 +0500

    update

--- client/src/components/hometmp.js
+++ client/src/components/hometmp.js
@@ -1,5 +1,6 @@
 import React from "react";
 // import "./assets/vendor/aos/aos.css";
+
 import "./assets/vendor/bootstrap/css/bootstrap.min.css";
 import "./assets/vendor/boxicons/css/boxicons.min.css";
 import "./assets/vendor/glightbox/css/glightbox.min.css";
